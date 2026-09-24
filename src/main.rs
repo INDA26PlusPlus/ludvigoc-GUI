fa1518b0569aa05b1601dd884e5c583a6984407d
ludvigockerman <ludvig.ockerman@gmail.com>
2026-09-24 19:27:11 +0200

    drawing a chess board

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 use ggez::event;
-use ggez::graphics::{self, Color};
+use ggez::graphics::{self, Color, Rect};
 use ggez::{Context, GameResult};
 use ggez::glam::*;
 
 struct MainState {
-    pos_x: f32,
 }
 
 impl MainState {
     fn new() -> GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 };
+        let s = MainState {  };
         Ok(s)
     }
 }
 
 impl event::EventHandler for MainState{
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
         Ok(())
     }
 
@@ -25,16 +23,30 @@ impl event::EventHandler for MainState{
             ctx,
             graphics::Color::from([0.1, 0.2, 0.3, 1.0]),
         );
-
-        let circle = graphics::Mesh::new_circle(
+        let light_square = graphics::Mesh::new_rectangle(
             ctx,
             graphics::DrawMode::fill(),
-            Vec2::new(0.0, 0.0),
-            100.0,
-            2.0,
+            Rect::new(0.0, 0.0, 70.0, 70.0),
             Color::WHITE,
         )?;
-        canvas.draw(&circle, Vec2::new(self.pos_x, 380.0));
+        let dark_square = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            Rect::new(0.0, 0.0, 70.0, 70.0),
+            Color::BLACK,
+        )?;
+        for rad in 0..8{
+            for kolumn in 0..8{
+                let square = if (rad + kolumn) % 2 == 0 {
+                    &light_square
+                } else {
+                    &dark_square
+                };
+                let x = kolumn as f32 * 70.0;
+                let y = rad as f32 * 70.0;
+                canvas.draw(square, Vec2::new(x, y));
+            }
+        }
 
         canvas.finish(ctx)?;
         Ok(())
